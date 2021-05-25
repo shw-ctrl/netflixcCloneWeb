c34c38d1f1eb31c23503c620c3a1266b7462b09f
shw-ctrl <59727496+shw-ctrl@users.noreply.github.com>
2021-05-25 15:09:36 +0530

    this is just a try to make clone web

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//select tab content item
+function selectItem(e){
+    removeBorder();
+    removeShow();
+    //add border to current class
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    // console.log(this.id);
+ const tabContentItem = document.querySelector(`#${this.id}-content`);
+ tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
+
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
